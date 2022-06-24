c4ac49553ed7d7e157259a752d80b3dc4331a5d1
timothynegron <negrt@farmingdale.edu>
2022-06-24 14:35:21 -0400

    adds main functions

---
+++ main.js
@@ -0,0 +1,62 @@
+// Get Port File from folder
+// Store as list object
+
+// 1 List Object 
+// O(n) Port Connection Objects
+// Port Conection Object = 1 Row
+
+function main(){
+    // Port Mapper Function
+
+    // if patchpanel{
+//  switch:
+//  PPA 
+//  PPB 
+//  etc..
+// then match port # to div id
+//}
+
+
+// if switch{
+//  switch:
+//  meraki
+//  avaya
+//  etc...
+//  then match port # to div id
+//}
+
+}
+
+
+function buildMerakiSwitch(){
+    // if found new switch
+    // build the switch
+}
+
+
+function buildPatchPanel(){
+
+}
+
+
+function buildAvayaSwitch(){
+
+}
+
+function portSwitch(){
+    // if match made
+    // turn port on
+    setToolTip();
+}
+
+
+function portClickListener(){
+    // if port clicked  
+    // change port color
+}
+
+
+function setToolTip(){
+    // if hover over port
+    // show tool tip over patch panel or port patched too
+}

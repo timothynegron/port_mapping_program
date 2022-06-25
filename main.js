0edffe06adb49257ffa18f388c66085298b7c123
timothynegron <negrt@farmingdale.edu>
2022-06-24 21:15:31 -0400

    adds more functions TODO

--- main.js
+++ main.js
@@ -5,29 +5,69 @@
 // O(n) Port Connection Objects
 // Port Conection Object = 1 Row
 
+// TODO: call functions in order
 function main(){
-    // Port Mapper Function
+    getPortData();
+    portMapper();
+    // drawDataVisualizations();
+    // buildRack();
+}
 
-    // if patchpanel{
-//  switch:
-//  PPA 
-//  PPB 
-//  etc..
-// then match port # to div id
-//}
+// ------- Phase 1
 
+// TODO: get the port data from the CSV file
+function getPortData(){
 
-// if switch{
-//  switch:
-//  meraki
-//  avaya
-//  etc...
-//  then match port # to div id
-//}
+}
+
+// TODO: map the ports to the divs on the switch or patch panel
+function portMapper(){
 
 }
 
 
+// ------- Phase 2
+
+// TODO: set the tool tips on each port (which patch panel or switch does it go to?)
+function setToolTip(){
+    // if hover over port
+    // show tool tip over patch panel or port patched too
+}
+
+// TODO: if port clicked, indicate somehow that port is on or off
+function portClickListener(){
+    // if port clicked  
+    // change port color
+}
+
+
+// ------- Phase 3
+
+// TODO: call build patchpanel or build switch accordingly
+function buildRack(){
+
+    // while{
+    //     if patchpanel{
+    //      switch:
+    //      PPA 
+    //      PPB 
+    //      etc..
+    //     then match port # to div id
+    //     }
+
+
+    //     if switch{
+    //      switch:
+    //      meraki
+    //      avaya
+    //      etc...
+    //      then match port # to div id
+    //     }
+    // }
+
+}
+
+// TODO: 
 function buildMerakiSwitch(){
     // if found new switch
     // build the switch
@@ -49,14 +89,9 @@ function portSwitch(){
     setToolTip();
 }
 
+// -------- Phase 4
 
-function portClickListener(){
-    // if port clicked  
-    // change port color
-}
+// TODO: build data visualizations
+function drawDataVisualizations(){
 
-
-function setToolTip(){
-    // if hover over port
-    // show tool tip over patch panel or port patched too
 }

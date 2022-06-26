c2a9404899b7b66a092e92d658669abf644fa91c
Timothy Negron <negrt@farmingdale.edu>
2022-06-26 13:11:49 -0400

    adds console logs

--- scripts/version1.js
+++ scripts/version1.js
@@ -1,16 +1,35 @@
-// ┌──────────────┐
-// │   Version 1  │	
-// └──────────────┘
+// ┌───────────────┐
+// │   Version 1   │	
+// └───────────────┘
+//
+// Get data from the CSV file and associate it with each port
+
+// ┌──────────────────────┐
+// │   Global Variables   │	
+// └──────────────────────┘
+
+const fileName = "meraki-sample-data.csv";
+
+
+// ┌───────────────┐
+// │   Functions   │	
+// └───────────────┘
 
 // TODO: get the port data from the CSV file
 function getPortData(){
-    // print data set to console
-    // const dataset = $.csv.toObjects(csv);
-    // console.log(dataset)
 
+    console.log("function: getPortData() started")
+
+    console.log("function: getPortData() ended")
 }
 
 // TODO: map the ports to the divs on the switch or patch panel
 function portMapper(){
+
+    console.log("function: portMapper() started")
+
+    console.log("function: portMapper() ended")
     
 }
+
+export default getPortData;

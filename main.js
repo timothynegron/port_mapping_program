036566b72a67fff72da8c8078f5a81a9290c4222
timothynegron <negrt@farmingdale.edu>
2022-07-04 23:51:35 -0400

    adds tooltip, toast, table

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // │   Global Variables   │	
 // └──────────────────────┘
 
+const toastDiv = document.getElementById('liveToast')
+const toast = new bootstrap.Toast(toastDiv)
+
 // ┌────────────────────┐
 // │   Call Functions   │	
 // └────────────────────┘
@@ -75,6 +78,8 @@ function portMapper(results){
 
     console.log("function: portMapper() started")
 
+    setToolTip()
+
     results.data.forEach(element => {
 
         const port = document.getElementById("meraki-port-" + element[0])
@@ -82,23 +87,39 @@ function portMapper(results){
         
         if(element[4] !== "Rack"){
 
-            if(portStatus === 'Open'){
+            if(portStatus !== 'Open'){
+
+                console.log("Notification: Port Connected")
+                port.style.backgroundColor = "#017BFE"
+                port.style.color = "white"
+                port.style.fontSize = "8pt"
+                port.innerHTML = element[1]
+                port.title = element[1] + " Port: " + element[2]
+            }
+
+            else if(portStatus === 'Open'){
 
                 console.log("Notification: Port Not Connected")
                 port.style.backgroundColor = "black"
                 port.style.color = "white"
                 port.style.fontSize = "8pt"
                 port.innerHTML = element[1]
-            }
-            if(portStatus !== 'Open'){
-
-                console.log("Notification: Port Connected")
-                port.style.backgroundColor = "#0066ff"
-                port.style.color = "white"
-                port.style.fontSize = "8pt"
-                port.innerHTML = element[1]
+                port.title = "No Patch Cable Connected"
             }
         }
     });
+
+    // document.getElementById("button-choose-file").style.backgroundColor = ""
+    // createTable()
+    toast.show()
     console.log("function: portMapper() ended")
+}
+
+function setToolTip(){
+    $('[data-toggle="tooltip"]').tooltip()
+}
+
+function createTable(){
+    // $("#csv-data-table").after("<thead><tr><th scope='col'>Switch Port Number</th><th scope='col'>Patch Panel</th></thead>")
+        // document.createElement("<thead><tr><th scope='col'>#</th><th scope='col'>First</th><th scope='col'>Last</th><th scope='col'>Handle</th><th scope='col'>This</th></tr></thead>"))
 }
